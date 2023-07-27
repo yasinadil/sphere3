746dc1a9df8983e00df773815b142e5705bb2953
yasinadil <adilyasin205@gmail.com>
2023-07-27 21:39:29 +0500

    google verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,13 +15,17 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
+            content="blockchain dapp, ethereum dapp, nextjs dapp, nextjs website, nft minting website, nft minting dapp, nft marketplace agency, blockchain agency, token staking agency, token presale, token presale agency, nft, wordpress agency, react agency, react business website, nextjs website "
           />
           <meta
             name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
+            content="Web development and Blockchain Agency - Your Business Partner"
           />
-          <meta name="author" content="" />
+          <meta name="author" content="Sphere3 Labs" />
+          <meta
+            name="google-site-verification"
+            content="F0SfLXU-8s_S_MbPYetqhPwtGtUJH2Ao-T6E674aTLo"
+          />
           <link rel="shortcut icon" href="/img/favicon.ico" />
           {/* Google Fonts */}
           <link
