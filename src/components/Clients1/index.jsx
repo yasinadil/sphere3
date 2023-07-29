33081fb7cadbfc9d3903dc774b50e158141c8141
yasinadil <adilyasin205@gmail.com>
2023-07-29 22:04:28 +0500

    logos

--- src/components/Clients1/index.jsx
+++ src/components/Clients1/index.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Clients1Data from "../../data/sections/clients1.json";
-import Split from "../Split";
 
 const Clients1 = ({ theme, subBG }) => {
   var first = Clients1Data.slice(0, Clients1Data.length / 2);
